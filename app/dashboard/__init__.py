b7f308c6d8cc07c08e00762a88848b08b0e587b3
Mukesh Prajapati <109510553+Mukeshp405@users.noreply.github.com>
2026-08-18 20:24:06 +0530

    Remove redirect_to_vercel function
    Removed redirect_to_vercel function from dashboard.

--- app/dashboard/__init__.py
+++ app/dashboard/__init__.py
@@ -3,11 +3,4 @@ from flask import Blueprint
 # ----------- Instiantiate Blueprint ----------- #
 dashboard = Blueprint('dashboard', __name__)
 
-@dashboard.route("/")
-def redirect_to_vercel():
-    return redirect(
-        "https://mukesh-prajapati.vercel.app/",
-        code=301
-    )
-
 from . import routes
